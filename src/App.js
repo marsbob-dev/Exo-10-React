2a17e157f672741c9063077ffe45fd4102912fa2
marsbob-dev <73162318+marsbob-dev@users.noreply.github.com>
2021-01-19 14:58:41 +0100

    exo terminé

--- src/App.js
+++ src/App.js
@@ -3,24 +3,46 @@ import './App.css';
 
 class App extends Component {
   state = {
-    paragraphe: "du lorem ipsum"
+    paragraphe: "du lorem ipsum",
+    titre: "Titre 2",
+    paragraphe2: "Je suis le second paragraphe"
   }
-onClick = (e) => {
-  e.target.innerHTML = "Titre 1"
-}
-changeParagraphe = () => {
-  this.setState({paragraphe: "Mon paragraphe est changé"})
-}
-render () {
+  onClick = (e) => {
+    e.target.innerHTML = "Titre 1"
+  }
+  changeParagraphe = () => {
+    this.setState({
+      paragraphe: "Mon paragraphe est changé"
+    })
+  }
+  changeViaInput = (e) => {
+    this.setState({
+      paragraphe2: e.target.value
+    })
+  }
+  changeViaBtn = (e) => {
+    let input = document.getElementById("inputTitre")
+    this.setState({
+      titre: input.value
+    })
+  }
+
+  render () {
 
     return (
       <div className="App">
         <h1 onClick={this.onClick}>Titre par défaut</h1>
+        <h2>{this.state.titre}</h2>
+        <input id="inputTitre" type="text"/>
+        <button onClick={this.changeViaBtn}>changer titre </button>
         <p>{this.state.paragraphe}</p>
         <button onClick={this.changeParagraphe}>Changer P</button>
+        <p>{this.state.paragraphe2}</p>
+        <input onInput={this.changeViaInput} type="text"/>
+        
       </div>
     )
   }
-  }
+}
 
 export default App;
