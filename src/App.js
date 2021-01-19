f7c615527717d16eafa1fa7f18d02bfaa63e1485
marsbob-dev <73162318+marsbob-dev@users.noreply.github.com>
2021-01-19 13:01:02 +0100

    void

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    paragraphe: "du lorem ipsum"
+  }
+onClick = (e) => {
+  e.target.innerHTML = "Titre 1"
 }
+changeParagraphe = () => {
+  this.setState({paragraphe: "Mon paragraphe est changé"})
+}
+render () {
+
+    return (
+      <div className="App">
+        <h1 onClick={this.onClick}>Titre par défaut</h1>
+        <p>{this.state.paragraphe}</p>
+        <button onClick={this.changeParagraphe}>Changer P</button>
+      </div>
+    )
+  }
+  }
 
 export default App;
